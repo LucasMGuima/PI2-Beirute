aeb6e5a555ffc15af555f0f433f3d77b398e74f1
ahmed mohamad bakri <ahmudi.ab@gmail.com>
2021-09-21 20:54:20 -0300

    Update Beirute.cpp

--- Beirute/Beirute/Beirute.cpp
+++ Beirute/Beirute/Beirute.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <allegro5/allegro.h>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	al_init();
+	ALLEGRO_DISPLAY* tela = al_create_display(800, 600);
+
+	while (true)
+		al_flip_display();
+	
+	std::cout << "Hello World!\n";
+
+	return 0;
+
 }
