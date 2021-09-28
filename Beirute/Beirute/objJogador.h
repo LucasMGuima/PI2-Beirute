0b8e4dcb977e0fb916561235a904684ca15021b4
Lucas <lucasdamata01@hotmail.com>
2021-09-27 21:55:31 -0300

    Update objJogador.h

--- Beirute/Beirute/objJogador.h
+++ Beirute/Beirute/objJogador.h
@@ -3,11 +3,12 @@ class objJogador
 {
 	private:
 		int TEMP_TAMANHO = 20;
+
 	public:
 		float x;
 		float y;
 		float velocidade;
-
+		
 		objJogador(float new_x, float new_y, float new_velocidade) {
 			x = new_x;
 			y = new_y;
@@ -18,22 +19,18 @@ class objJogador
 			if (al_key_down(&ks, ALLEGRO_KEY_W)) {
 				float new_y = y - velocidade;
 				if (new_y > 0) y = new_y;
-				//y -= velocidade;
 			}
 			if (al_key_down(&ks, ALLEGRO_KEY_S)) {
 				float new_y = y + velocidade;
 				if (new_y + TEMP_TAMANHO < al_get_display_height(tela)) y = new_y;
-				//y += velocidade;
 			}
 			if (al_key_down(&ks, ALLEGRO_KEY_A)) {
 				float new_x = x - velocidade;
 				if (new_x > 0) x = new_x;
-				//x -= velocidade;
 			}
 			if (al_key_down(&ks, ALLEGRO_KEY_D)) {
 				float new_x = x + velocidade;
 				if (new_x + TEMP_TAMANHO < al_get_display_width(tela)) x = new_x;
-				//x += velocidade;
 			}
 		}
 };
