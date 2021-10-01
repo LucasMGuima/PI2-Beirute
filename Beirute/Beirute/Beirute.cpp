8a213bacc5fcb10bd8e1d18cb3074f4543887825
Lucas <lucasdamata01@hotmail.com>
2021-10-01 08:17:44 -0300

    inimigo aparece na tela

--- Beirute/Beirute/Beirute.cpp
+++ Beirute/Beirute/Beirute.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
 
 using namespace std;
+
+struct inimigo {
+	float x, y;
+	int tipo_caminho;
+
+	void mover() {
+		//logica do movimento aqui;
+	}
+
+	void desenhar() {
+		al_draw_filled_rectangle(x, y, x + 20, y + 20, al_map_rgb(255, 0, 0));
+		//se redesenha na tela com base na nova posição
+	}
+};
 
 void iniciar(bool teste, const char *descricao) {
 	if (teste) return;
@@ -12,6 +27,7 @@ void iniciar(bool teste, const char *descricao) {
 int main()
 {
 	iniciar(al_init(), "Allegro");
+	iniciar(al_init_primitives_addon(), "Addon Primitivas");
 
 	ALLEGRO_DISPLAY* tela = al_create_display(800, 600);
 	iniciar(tela, "Tela");
@@ -19,16 +35,30 @@ int main()
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
 	iniciar(queue, "Event Queue");
 
+	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+	iniciar(timer, "Timer");
+
 	al_register_event_source(queue, al_get_display_event_source(tela));
+	al_register_event_source(queue, al_get_timer_event_source(timer));
 
 	ALLEGRO_EVENT event;
 
 	bool done = false;
 
+	inimigo inimigo01;
+	inimigo01.x = 400;
+	inimigo01.y = 300;
+	inimigo01.tipo_caminho = 1;
+
+	al_start_timer(timer);
 	while (true) {
 		al_wait_for_event(queue, &event);
 
 		switch (event.type) {
+			case ALLEGRO_EVENT_TIMER:
+				inimigo01.mover();
+				break;
+		
 			case ALLEGRO_EVENT_DISPLAY_CLOSE:
 				done = true;
 				break;
@@ -36,6 +66,8 @@ int main()
 
 		if (done) break;
 
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		inimigo01.desenhar();
 		al_flip_display();
 	}
 
