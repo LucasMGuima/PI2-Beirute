b634be9c82ae200f65135d739b0c44e0d433b45c
Lucas <lucasdamata01@hotmail.com>
2021-10-01 07:53:14 -0300

    Janela fecha
    O botão de fechar a janela está funcionando, encerrando a aplicação quando apertado.

--- Beirute/Beirute/Beirute.cpp
+++ Beirute/Beirute/Beirute.cpp
@@ -3,17 +3,44 @@
 
 using namespace std;
 
+void iniciar(bool teste, const char *descricao) {
+	if (teste) return;
+
+	printf("Nao foi pocivel iniciar %s\n", descricao);
+}
 
 int main()
 {
-	al_init();
-	ALLEGRO_DISPLAY* tela = al_create_display(800, 600);
+	iniciar(al_init(), "Allegro");
 
-	while (true)
+	ALLEGRO_DISPLAY* tela = al_create_display(800, 600);
+	iniciar(tela, "Tela");
+
+	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+	iniciar(queue, "Event Queue");
+
+	al_register_event_source(queue, al_get_display_event_source(tela));
+
+	ALLEGRO_EVENT event;
+
+	bool done = false;
+
+	while (true) {
+		al_wait_for_event(queue, &event);
+
+		switch (event.type) {
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				done = true;
+				break;
+		}
+
+		if (done) break;
+
 		al_flip_display();
-	
-	std::cout << "Hello World!\n";
+	}
+
+	al_destroy_display(tela);
+	al_destroy_event_queue(queue);
 
 	return 0;
-
 }
