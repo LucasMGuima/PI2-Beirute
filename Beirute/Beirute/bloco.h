ffd5074a3e1c56636b5920752d2534c9d1f76938
Kevin Almeida <84411377+KevinAlmeidaMelo@users.noreply.github.com>
2021-11-23 17:21:02 -0300

    Update bloco.h

--- Beirute/Beirute/bloco.h
+++ Beirute/Beirute/bloco.h
@@ -23,34 +23,5 @@ struct bloco
 
 
 		
-		void desenharA() {
-			ALLEGRO_KEYBOARD_STATE ks;
-			al_get_keyboard_state(&ks);
-			ALLEGRO_BITMAP* W, * A, * D, * S;
-			S = al_load_bitmap("seringa_baixo.png");
-			A = al_load_bitmap("seringa_esq.png");
-			W = al_load_bitmap("seringa_cima.png");
-			D = al_load_bitmap("seringa_dir.png");
-
-			if (al_key_down(&ks, ALLEGRO_KEY_W)) {
-				al_draw_bitmap(W, x, y, 0);
-
-			}
-			else if (al_key_down(&ks, ALLEGRO_KEY_D)) {
-				al_draw_bitmap(D, x, y, 0);
-			}
-			else if (al_key_down(&ks, ALLEGRO_KEY_A)) {
-				al_draw_bitmap(A, x, y, 0);
-			}
-			else if (al_key_down(&ks, ALLEGRO_KEY_S)) {
-				al_draw_bitmap(S, x, y, 0);
-			}
-		}
-
-		void desenharCor(int r, int g, int b) {
-			al_draw_filled_rectangle(x, y, x + 20, y + 20, al_map_rgb(r, g, b));
-		}
-
-		
 
  };
